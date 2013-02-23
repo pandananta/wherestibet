652dce4a503e63a3331f76b0c11f87058fe7e048
Ananta Pandey <pandananta@gmail.com>
2013-02-23 15:33:24 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wherestibet::Application.config.secret_token = 'aecfb494793199deb75779dbda4bbba494ea76c4b53257386fd7d4483b9c06ac4d143e82a937aeb1b1c4e67662d9baf94cc8dc4e55f29ca90b0a565368d90cf1'
