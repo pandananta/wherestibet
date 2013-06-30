8ca1160a67e4446e618a7acccb32a2a1fbcd9722
Ananta Pandey <pandananta@gmail.com>
2013-06-30 19:35:52 -0400

    integrated gmaps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,115 +10,40 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery-1.7.1.min.js
 //= require jquery_ujs
-//= require bootstrap
-//= require jquery-jvectormap-1.2.2.min
-//= require jquery-jvectormap-world-mill-en
+//= require bootstrap.js
 //= require_tree .
 
+function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(0,0),
+          zoom: 4,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+        
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h2 id="firstHeading" class="firstHeading">Tibet is right here!</h2>'+
+            '<iframe src="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-ash3/562674_10151020009322986_336046691_n.jpg"></iframe>'+
+            '</div>';
 
-// $(document).ready(function() {
-//    var markerList=[];
-//  });
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+        });
 
-// $(function setMarkers(){
-// 	$('#world-map').vectorMap('set', 'markers', {latLng: [7.35, 134.46], name: 'Tenzin'});
-// });
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(0,0),
+            map: map,
+            title:"Hello World!"
+        });
 
-// $(function(){
-//   $('#world-map').vectorMap({
-//     map: 'world_mill_en',
-//     scaleColors: ['#C8EEFF', '#0071A4'],
-//     normalizeFunction: 'polynomial',
-//     hoverOpacity: 0.7,
-//     hoverColor: false,
-//     markerStyle: {
-//       initial: {
-//         fill: '#F8E23B',
-//         stroke: '#383f47'
-//       }
-//     },
-    
-//     markers: [
-//       {latLng: [7.35, 134.46], name: 'Tenzin'},
-//       {latLng: [42.5, 1.51], name: 'Penpa'},
-//       {latLng: [14.01, -60.98], name: 'Tenzin'},
-//       {latLng: [6.91, 158.18], name: 'Tashi'},
-//       {latLng: [1.3, 103.8], name: 'Sonam'},
-//       {latLng: [1.46, 173.03], name: 'Kiribati'},
-//       {latLng: [-21.13, -175.2], name: 'Tonga'},
-//       {latLng: [15.3, -61.38], name: 'Tashi'},
-//       {latLng: [-20.2, 57.5], name: 'Tashi'},
-//       {latLng: [26.02, 50.55], name: 'Sonam'}
-//     ]
-//   });
-// });
-
-$(function(){
-  var map,
-      markerIndex = 0,
-      markersCoords = {};
-
-  map = new jvm.WorldMap({
-      map: 'world_mill_en',
-      markerStyle: {
-        initial: {
-          fill: 'red',
-          stroke: 'red',
-          r: 2
-        }
-      },
-      container: $('#world-map'),
-      onMarkerLabelShow: function(e, label, code){
-        map.label.text(markersCoords[code].lat.toFixed(2)+', '+markersCoords[code].lng.toFixed(2));
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+          // window.location.href = 'http://www.google.com';
+        });
       }
-      ,onMarkerClick: function(e, code){
-        map.removeMarkers([code]);
-        map.label.hide();
-      }
-  });
-
-  // map.container.click(function(e){
-  //     var latLng = map.pointToLatLng(e.offsetX, e.offsetY),
-  //         targetCls = $(e.target).attr('class');
-
-  //     if (latLng && (!targetCls || (targetCls && $(e.target).attr('class').indexOf('jvectormap-marker') === -1))) {
-  //       markersCoords[markerIndex] = latLng;
-  //       map.addMarker(markerIndex, {latLng: [latLng.lat, latLng.lng]});
-  //       markerIndex += 1;
-  //     }
-  // });
-
-
-
-  $('#locabutton').click(function(){
-    if (navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(showPosition,showError);
-    }
-    else{x.innerHTML="Geolocation is not supported by this browser."};
-  });
-
-  function showPosition(position){
-      console.log("Your position is "+position.coords.latitude+","+position.coords.longitude);
-      map.addMarker(markerIndex, {latLng: [position.coords.latitude, position.coords.longitude]});
-      markerIndex += 1;
-  };
-
-  function showError(error){
-    switch(error.code) {
-      case error.PERMISSION_DENIED:
-        x.innerHTML="User denied the request for Geolocation."
-        break;
-      case error.POSITION_UNAVAILABLE:
-        x.innerHTML="Location information is unavailable."
-        break;
-      case error.TIMEOUT:
-        x.innerHTML="The request to get user location timed out."
-        break;
-      case error.UNKNOWN_ERROR:
-        x.innerHTML="An unknown error occurred."
-        break;
-    };
-  };
-});
+      google.maps.event.addDomListener(window, 'load', initialize);
