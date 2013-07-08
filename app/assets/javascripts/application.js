ffa322728bdd889761275047fb111bd288014aaa
Ananta Pandey <pandananta@gmail.com>
2013-07-07 21:47:25 -0400

    Iterate through stories, plot on map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,15 +10,15 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-1.7.1.min.js
 //= require jquery_ujs
 //= require bootstrap.js
 //= require_tree .
 
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(0,0),
-    zoom: 4,
+    center: new google.maps.LatLng(30,0),
+    zoom: 2,
+    minZoom: 2,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
@@ -34,16 +34,26 @@ function initialize() {
       content: contentString,
   });
 
-  var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(0,0),
+  for (var i=0; i<window.stories.length; i++){ 
+    markerLat = window.stories[i]["latitude"];
+    markerLong = window.stories[i]["longitude"];
+    markerID = window.stories[i]["id"]
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(markerLat,markerLong),
       map: map,
       title:"Hello World!"
-  });
+    });
+    initMarker(marker, i);
+  }
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-    // window.location.href = '/stories/1';
-  });
+  function initMarker(marker, number) {
+    google.maps.event.addListener(marker, 'click', function(e) {
+      // infowindow.open(map,markers[i]);
+      window.location.href = ("/stories/" + window.stories[number]["id"]);
+    });
+  }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,11 @@
 class Story < ActiveRecord::Base
   attr_accessible :age, :agreeTerms, :author, :email, :gender, :latitude, :longitude, :narrative
+
+  def as_json opts = {}
+    {
+      id: self.id,
+      latitude: self.latitude,
+      longitude: self.longitude,
+    }
+  end
 end
