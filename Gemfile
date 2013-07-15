dc896809bdf0724b13ab3ee5e55bc150631493ec
Ananta Pandey <pandananta@gmail.com>
2013-07-14 23:25:50 -0400

    added geocoder gem

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'rails_admin'
+gem 'geocoder'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+ 	protect_from_forgery
+ 	
+ 	# Override default behavior for testing on local machine 
+	def remote_ip
+		if request.remote_ip == '127.0.0.1'
+			# Random remote address for testing on develop
+			"#{rand(255)}.#{rand(255)}.#{rand(255)}.#{rand(255)}"
+		else
+			request.remote_ip
+		end
+	end
 end
