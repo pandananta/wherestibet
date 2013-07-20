a3a8a84c23a9ab25595f039dc8007d713f4ed485
Ananta Pandey <pandananta@gmail.com>
2013-07-20 04:32:24 -0400

    added cookies, so zoom is persistant

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,7 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+  loadMapState();
 
   for (var i=0; i<window.stories.length; i++){ 
     markerLat = window.stories[i]["latitude"];
@@ -50,7 +51,57 @@ function initialize() {
   if ($.url(window.location.href).param('n')) {
     $.fancybox( {href: ('/stories/' + $.url(window.location.href).param('n')) , type:'iframe'})
   }
+  google.maps.event.addListener(map, 'tilesloaded', tilesLoaded);
+  function tilesLoaded() {
+    google.maps.event.clearListeners(map, 'tilesloaded');
+    google.maps.event.addListener(map, 'zoom_changed', saveMapState);
+    google.maps.event.addListener(map, 'dragend', saveMapState);
+  }
+  function saveMapState() { 
+    var mapZoom=map.getZoom(); 
+    var mapCentre=map.getCenter(); 
+    var mapLat=mapCentre.lat(); 
+    var mapLng=mapCentre.lng(); 
+    var cookiestring=mapLat+"_"+mapLng+"_"+mapZoom; 
+    setCookie("myMapCookie",cookiestring, 30); 
+  } 
+
+  function loadMapState() { 
+      var gotCookieString=getCookie("myMapCookie"); 
+      var splitStr = gotCookieString.split("_");
+      var savedMapLat = parseFloat(splitStr[0]);
+      var savedMapLng = parseFloat(splitStr[1]);
+      var savedMapZoom = parseFloat(splitStr[2]);
+      if ((!isNaN(savedMapLat)) && (!isNaN(savedMapLng)) && (!isNaN(savedMapZoom))) {
+          map.setCenter(new google.maps.LatLng(savedMapLat,savedMapLng));
+          map.setZoom(savedMapZoom);
+      }
+  }
+
+  function setCookie(c_name,value,exdays) {
+      var exdate=new Date();
+      exdate.setDate(exdate.getDate() + exdays);
+      var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+      document.cookie=c_name + "=" + c_value;
+  }
+
+  function getCookie(c_name) {
+      var i,x,y,ARRcookies=document.cookie.split(";");
+      for (i=0;i<ARRcookies.length;i++)
+      {
+        x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+        y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+        x=x.replace(/^\s+|\s+$/g,"");
+        if (x==c_name)
+          {
+          return unescape(y);
+          }
+        }
+      return "";
+  }  
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
 
 
