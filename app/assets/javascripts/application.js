5d5c0eed878d2c4c90514787c6c93936fde9d274
Ananta Pandey <pandananta@gmail.com>
2013-07-19 22:55:51 -0400

    going to a story's URL will open that story in its modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+ // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -13,6 +13,7 @@
 //= require jquery_ujs
 //= require bootstrap.js
 //= require jquery.fancybox
+//= require purl
 //= require_tree .
 
 function initialize() {
@@ -51,9 +52,13 @@ function initialize() {
   function initMarker(marker, number) {
     google.maps.event.addListener(marker, 'click', function(e) {
       // infowindow.open(map,markers[i]);
-      // window.location.href = ("/stories/" + window.stories[number]["id"]);
-      $.fancybox( {href: ('/stories/'+window.stories[number]["id"]), type:'iframe'});
+      window.location.href = ("/?story_id=" + window.stories[number]["id"]);
+      // $.fancybox( {href: ('/stories/'+window.stories[number]["id"]), type:'iframe'});
     });
+  }
+
+  if ($.url(window.location.href).param('story_id')) {
+    $.fancybox( {href: ('/stories/' + $.url(window.location.href).param('story_id')) , type:'iframe'})
   }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
